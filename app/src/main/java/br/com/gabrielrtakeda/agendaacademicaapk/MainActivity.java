637a42b7ceac9832de935435db0a882409f453bc
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2016-03-30 06:52:19 -0300

    Subir apk webview.

---
+++ app/src/main/java/br/com/gabrielrtakeda/agendaacademicaapk/MainActivity.java
@@ -0,0 +1,28 @@
+package br.com.gabrielrtakeda.agendaacademicaapk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://fathomless-ocean-55981.herokuapp.com/");
+//        mWebView.loadUrl("http://192.168.0.59:3000/");
+    }
+}
